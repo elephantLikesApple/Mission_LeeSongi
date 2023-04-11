27eade6405e4dff8d46eaad7d3840a78bdec86a3
elephantLikesApple <elephant890@naver.com>
2023-04-11 23:44:06 +0900

    Test : Add new test data

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -26,7 +26,8 @@ public class NotProd {
             Member memberUser4 = memberService.join("user4", "1234").getData();
 
             Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733199206").getData();
-            Member memberUser6ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE_109774549589647611854").getData();
+            Member memberUser6ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__109774549589647611854").getData();
+            Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__8J-EUJrBoSIJixy0UghTLpbInc_3C5cHK1-xCNjTB94").getData();
 
             instaMemberService.connect(memberUser2, "insta_user2", "M");
             instaMemberService.connect(memberUser3, "insta_user3", "W");
